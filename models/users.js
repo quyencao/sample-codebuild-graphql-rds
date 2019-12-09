6083dc974e18817ecd7477c44483c85c6b6ae327
quyencao <boy20195@gmail.com>
2019-12-09 10:42:47 +0700

    new commit

--- models/users.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define('users', {id:{type:DataTypes.NUMBER,allowNull:false,primaryKey:true,autoIncrement:true},email:{type:DataTypes.TEXT,allowNull:false},username:{type:DataTypes.TEXT,allowNull:true},gender:{type:DataTypes.TEXT,allowNull:true}}, {timestamps: false, tableName: 'users'})
-  Users.associate = function(models) {
-    // associations can be defined here
-  }
-  return Users
-}
--- schema.js
+++ schema.js
@@ -5,16 +5,11 @@ type Mutation {
   createTodo(input: CreateTodoInput!): Todo
   updateTodo(id: ID!, input: UpdateTodoInput!): Todo
   deleteTodo(id: ID!): Boolean
-  createUser(input: CreateUserInput!): User
-  updateUser(id: ID!, input: UpdateUserInput!): User
-  deleteUser(id: ID!): Boolean
 }
 
 type Query {
   getTodo(id: ID!): Todo
   getTodos: [Todo!]
-  getUser(id: ID!): User
-  getUsers: [User!]
 }
 
 input CreateTodoInput {
@@ -27,30 +22,11 @@ input UpdateTodoInput {
   completed: Boolean
 }
 
-input CreateUserInput {
-  email: String!
-  username: String
-  gender: String
-}
-
-input UpdateUserInput {
-  email: String
-  username: String
-  gender: String
-}
-
 
 type Todo {
   id: ID!
   text: String!
   completed: Boolean
-}
-
-type User {
-  id: ID!
-  email: String!
-  username: String
-  gender: String
 }
 
 `;
